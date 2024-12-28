c4fc37258f3998d57399b9406f44acf6a0a9b9aa
Nikita <NVnagornyi@yandex.ru>
2024-12-28 03:12:34 +0300

    создана ветка, верстка профиля без добавления кнопок перехода

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Никита Нагорный on 27.12.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Никита Нагорный on 28.12.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var avatarPhoto: UIImageView!
+    
+    // MARK: - IBAction
+    
+    @IBAction func logoutButtonClicked(_ sender: Any) {
+        
+    }
+}
