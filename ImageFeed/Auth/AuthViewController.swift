b41c6c52d95044d33239e37af56168ffafa748b3
Nikita <NVnagornyi@yandex.ru>
2025-01-25 16:22:42 +0300

    add login window and login class

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Никита Нагорный on 25.01.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
