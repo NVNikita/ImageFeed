f7144bd82bc5e8a2bb4a2f9dd6f2b7ffa21acf55
Nikita <NVnagornyi@yandex.ru>
2025-03-02 13:48:48 +0300

    fixed #2 ImageListService

--- ImageFeed/ImagesListService.swift
+++ ImageFeed/ImagesListService.swift
@@ -1,8 +1,7 @@
 //
 //  ImagesListService.swift
 //  ImageFeed
-//
-//
+
 
 import Foundation
 import UIKit
@@ -56,15 +55,19 @@ final class ImagesListService {
     private var lastLoadedPage: Int?
     private var task: URLSessionTask?
     private var oa2Token = OAuth2TokenStorage.shared.token
+    private let dateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        return formatter
+    }()
     
-    func fetchPhotosNextPage() {
-        
+    func fetchPhotosNextPage(completion: @escaping (Result<[Photo], Error>) -> Void) {
         guard task == nil else { return }
         
         let nextPage = (lastLoadedPage ?? 0) + 1
         
         guard let url = URL(string: "https://api.unsplash.com/photos?page=\(nextPage)") else {
             print("[ImagesListService]: [Error URL")
+            completion(.failure(NetworkImageListServiceError.invalidURL))
             return
         }
         
@@ -74,54 +77,46 @@ final class ImagesListService {
         if let token = oa2Token {
             request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         } else {
+            completion(.failure(NetworkImageListServiceError.invalidToken))
             print("[ImagesListService]: [Error request with token]")
             return
         }
         
-        let task = URLSession.shared.dataTask(with: request) { [ weak self ] data, response, error in
+        let task = URLSession.shared.objectTask(for: request) { [weak self]
+            (result: Result<[PhotoResult], Error>) in
             guard let self else { return }
             
-            if let error {
-                self.task = nil
-                print("[ImagesListService]: [Error \(error)]")
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                print("[ImagesListService]: [Error invalid response]")
-                self.task = nil
-                return
-            }
-            
-            guard let data else {
-                self.task = nil
-                print("[ImagesListService]: [Error data]")
-                return
-            }
-            
-            do {
-                let newPhotos = try JSONDecoder().decode([PhotoResult].self, from: data).map { photoResult in
+            switch result {
+            case .success(let photoResults):
+                let newPhotos = photoResults.map { photoResult in
                     Photo(id: photoResult.id,
                           size: CGSize(width: photoResult.width, height: photoResult.height),
-                          createdAt: ISO8601DateFormatter().date(from: photoResult.createdAt),
+                          createdAt: self.dateFormatter.date(from: photoResult.createdAt),
                           welcomeDescription: photoResult.description,
                           thumbImageURL: photoResult.urls.thumb,
                           largeImageURL: photoResult.urls.full,
-                          isLiked: photoResult.likedByUser)}
+                          isLiked: photoResult.likedByUser)
+                }
                 
                 DispatchQueue.main.async {
                     self.photos.append(contentsOf: newPhotos)
                     self.lastLoadedPage = nextPage
-                    
-                    NotificationCenter.default.post(name: ImagesListService.didChangeNotification,
-                                                    object: self)}
-                
-            } catch {
-                print("[ImagesListService]: [Error decoding]")
+                    NotificationCenter.default.post(name: ImagesListService.didChangeNotification, object: self)
+                    completion(.success(newPhotos))
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
             }
             self.task = nil
         }
         self.task = task
         task.resume()
     }
+}
+
+enum NetworkImageListServiceError: Error {
+    case invalidURL
+    case invalidToken
 }
