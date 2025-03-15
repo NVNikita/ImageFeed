1f6afa12b4c675e40f0bf72e44c05724140696e5
Nikita <NVnagornyi@yandex.ru>
2025-03-15 03:32:37 +0300

    add AuthHelper and refactoring WebViewPresenter

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -34,7 +34,8 @@ final class AuthViewController: UIViewController {
                 assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
                 return
             }
-            let webViewPresenter = WebViewPresenter()
+            let authHelper = AuthHelper()
+            let webViewPresenter = WebViewPresenter(authHelper: authHelper)
             webViewViewController.presenter = webViewPresenter
             webViewPresenter.view = webViewViewController
             webViewViewController.delegate = self
@@ -45,7 +46,6 @@ final class AuthViewController: UIViewController {
     
     // MARK: Private Methods
     
-    // настройка кнопки выхода
     private func configureBackButton() {
         navigationController?.navigationBar.backIndicatorImage = UIImage(named: "Backward")
         navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "Backward")
--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -19,31 +19,20 @@ enum WebViewConstants {
 }
 
 final class WebViewPresenter: WebViewPresenterProtocol {
+    
     weak var view: WebViewViewControllerProtocol?
+    var authHelper: AuthHelperProtocol
+    
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
+    }
     
     func viewDidLoad() {
         
-        guard var urlComponents = URLComponents(
-            string: WebViewConstants.unsplashAuthorizeURLString) else {
-            print("Error init url unsplash")
-            return
-        }
+        guard let request = authHelper.authRequest() else { return }
         
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        
-        guard let url = urlComponents.url else {
-            print("Error url unsplash for webView")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        didUpdateProgressValue(0)
         view?.load(request: request)
+        didUpdateProgressValue(0)
     }
     
     func didUpdateProgressValue(_ newValue: Double) {
@@ -59,15 +48,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func code(from url: URL) -> String? {
-        if
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
+        authHelper.code(from: url)
     }
 }
